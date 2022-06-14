7b49f2b9ab9dc1cf258b3cd29186d59869f28be2
Cynthia Teeters <cynthiateeters@gmail.com>
2022-06-14 16:25:51 -0400

    Add blog articles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
+const { DateTime } = require('luxon');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/style.css');
+
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
+
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'LLLL dd, yyyy '
+    );
+  });
+
   return {
     dir: {
       input: 'src/',
